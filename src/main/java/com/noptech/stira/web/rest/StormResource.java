88b873ea4b32905f343d22893838227a9cd7d6cc
Madsn <m@madsn.net>
2016-03-10 23:33:56 +0100

    Quick prototype - fetch severity and status from storm

---
+++ src/main/java/com/noptech/stira/web/rest/StormResource.java
@@ -0,0 +1,64 @@
+package com.noptech.stira.web.rest;
+
+import com.codahale.metrics.annotation.Timed;
+import com.noptech.stira.web.rest.dto.StormStatusDTO;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Controller for test purposes
+ */
+@RestController
+@RequestMapping("/api")
+public class StormResource {
+
+    private final Logger log = LoggerFactory.getLogger(AccountResource.class);
+
+    @RequestMapping(value = "/storm/{ticketIdParam:.+}",
+        method = RequestMethod.GET,
+        produces = MediaType.APPLICATION_JSON_VALUE)
+    @Timed
+    public StormStatusDTO getStatus(@PathVariable String ticketIdParam) {
+        StormStatusDTO stormStatus = new StormStatusDTO();
+
+        WebDriver driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://customer.tdchosting.com");
+
+        WebElement usernameField = driver.findElement(By.id("email"));
+        WebElement passwordField = driver.findElement(By.id("password"));
+        usernameField.sendKeys("");
+        passwordField.sendKeys("");
+
+        WebElement form = driver.findElement(By.cssSelector(".loginBox input[type=submit]"));
+        form.submit();
+
+        // Find element - ensure page is loaded
+        driver.findElement(By.cssSelector("a[title=Tickets]"));
+
+        driver.navigate().to("https://customer.tdchosting.com/tickets/tickets-details/?id=" + ticketIdParam);
+
+        WebElement priorityElem = driver.findElement(By.cssSelector("div.tick-info-table:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(1) > td:nth-child(2)"));
+        short priority = Short.parseShort(priorityElem.getText().substring(4));
+        stormStatus.setPriority(priority);
+
+        WebElement statusElem = driver.findElement(By.cssSelector("div.tick-info-table:nth-child(1) > table:nth-child(2) > tbody:nth-child(1) > tr:nth-child(5) > td:nth-child(2)"));
+        stormStatus.setStatus(statusElem.getText());
+
+        driver.close();
+        return stormStatus;
+    }
+}
---
+++ src/main/java/com/noptech/stira/web/rest/dto/StormStatusDTO.java
@@ -0,0 +1,35 @@
+package com.noptech.stira.web.rest.dto;
+
+public class StormStatusDTO {
+    private long id;
+    private short priority;
+    private String status;
+
+    public StormStatusDTO() {
+        this.id = 5;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public short getPriority() {
+        return this.priority;
+    }
+
+    public void setPriority(short priority) {
+        this.priority = priority;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
